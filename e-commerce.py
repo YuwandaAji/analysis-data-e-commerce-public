a869951fcc604c6c190b16d1d75199d91c3e2bf3
YuwandaAji <157210110+YuwandaAji@users.noreply.github.com>
2024-12-22 22:00:43 +0700

    Update e-commerce.py

--- e-commerce.py
+++ e-commerce.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit  as st
+import openpyxl
 from babel.numbers import format_currency
 
 def create_monthly_order(df):
